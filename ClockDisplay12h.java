17b4b13b512c2a83bf8316718efb40249e1068d9
Juan Carlos Aragón <juankar.md@gmail.com>
2014-11-21 14:10:20 +0100

    Modificado la clase ClockDisplay12h que contenia un reloj en formato 12h para que a mayores muestre la fecha en formato dia/mes/año aparte de que funcione

--- ClockDisplay12h.java
+++ ClockDisplay12h.java
@@ -12,6 +12,9 @@ public class ClockDisplay12h
 {
     private NumberDisplay hours;
     private NumberDisplay minutes;
+    private NumberDisplay years;
+    private NumberDisplay month;
+    private NumberDisplay day;
     private String displayString; // simulates the actual display
     private boolean estate;
     private String estateDisplay;
@@ -23,8 +26,11 @@ public class ClockDisplay12h
    {
        hours = new NumberDisplay(13);
        minutes = new NumberDisplay(60);
+       years = new NumberDisplay(100);
+       month = new NumberDisplay(13);
+       day = new NumberDisplay(31);
        estate = true;
-       setTime(12,0);
+       setTime(12,0,14,11,21);
     }
  
     /**
@@ -32,12 +38,16 @@ public class ClockDisplay12h
     * creates a new clock set at the time specified by the
     * parameters.
     */
-   public ClockDisplay12h(int hour, int minute)
+   public ClockDisplay12h(int hour, int minute, int nYear1, int nMonth1, int nDay1)
    {
        hours = new NumberDisplay(13);
        minutes = new NumberDisplay(60);
+       years = new NumberDisplay(100);
+       month = new NumberDisplay(13);
+       day = new NumberDisplay(31);
+       
        estate = true;
-       setTime(hour, minute);
+       setTime(hour, minute, nYear1, nMonth1, nDay1);
     }
  
     /**
@@ -54,21 +64,44 @@ public class ClockDisplay12h
             }
             if (hours.getValue() == 12){
                 estate = !estate;
+                if (estate = true){
+                    changeDay();
+                }
             }
 
         }
        
+       
         updateDisplay();
+    }
+    //Metodo que hace correr la fecha un día
+    private void changeDay(){
+        day.increment();
+        if (day.getValue() == 0){
+            day.setValue(1);
+        }
+        if(day.getValue() == 1){
+            month.increment();
+            if(month.getValue() == 0){
+                month.setValue(1);
+            }
+            if(month.getValue() == 1){
+                years.increment();
+            }
+        }
     }
  
     /**
     * Set the time of the display to the specified hour and
     * minute.
     */
-   public void setTime(int hour, int minute)
+   public void setTime(int hour, int minute, int nYear, int nMonth, int nDay)
    {
        hours.setValue(numericControl(hour));
        minutes.setValue(minute);
+       years.setValue(nYear);
+       month.setValue(nMonth);
+       day.setValue(nDay);
        updateDisplay();
     }
  
@@ -92,7 +125,7 @@ public class ClockDisplay12h
         }
         
        displayString = hours.getDisplayValue() + ":" +
-       minutes.getDisplayValue() + estateDisplay;
+       minutes.getDisplayValue() + estateDisplay + " " + day.getDisplayValue() + "/" + month.getDisplayValue() + "/" + "20" + years.getDisplayValue();
     }
     private int numericControl(int n1){
         if (n1>12){
